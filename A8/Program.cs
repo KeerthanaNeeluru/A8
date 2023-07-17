0ac9addcf4b436d9277a147c027745a1c305cfb8
keerthana <keerthananeeluru2001@gmail.com>
2023-07-17 17:36:53 +0530

    commit:2

--- A8/Program.cs
+++ A8/Program.cs
@@ -26,30 +26,36 @@ namespace A8
         static void Main(string[] args)
         {
             calc obj = new calc();
-            Console.WriteLine("Please Enter your role");
-            string role = Console.ReadLine();
-            switch (role)
+            char ch;
+            do
             {
-                case "HR":
-                    Console.WriteLine($"Monthly salary of the {role} is:\t" + obj.Calculate());
-                    break;
-                case "Admin":
-                    Console.WriteLine("Please enter your projrctHandles");
-                    int p1 = int.Parse(Console.ReadLine());
-                    Console.WriteLine($"Monthly salary of the {role} is:\t" + obj.Calculate(p1));
-                    break;
-                case "SoftwareDeveloper":
-                    Console.WriteLine("Please enter your projrctHandles");
-                    int p2 = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Please enter your extras");
-                    int extras = int.Parse(Console.ReadLine());
-                    Console.WriteLine($"Monthly salary of the {role} is:\t" + obj.Calculate(p2, extras));
-                    break;
-                default:
-                    Console.WriteLine("please Enter valid role");
-                    break;
-            }
-            Console.ReadKey();
+                Console.WriteLine("Please Enter your role\n  Human Resourse ---> HR\n Admin department--->ADMIN\n SoftwareDeveloper--->SD ");
+                string role = Console.ReadLine().ToUpper();
+                switch (role)
+                {
+                    case "HR":
+                        Console.WriteLine($"Monthly salary of the {role} is:\t" + obj.Calculate());
+                        break;
+                    case "ADMIN":
+                        Console.WriteLine("Please enter number of  projects Handled");
+                        int p1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Monthly salary of the {role} is:\t" + obj.Calculate(p1));
+                        break;
+                    case "SD":
+                        Console.WriteLine("Please enter number of projects Handled");
+                        int p2 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Please enter your extras");
+                        int extras = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Monthly salary of the {role} is:\t" + obj.Calculate(p2, extras));
+                        break;
+                    default:
+                        Console.WriteLine("please Enter valid role");
+                        break;
+                }
+                Console.WriteLine("if you want to continue press y");
+                ch = char.Parse(Console.ReadLine());
+            } while (ch == 'y');
+             Console.ReadKey();
 
         }
     }
